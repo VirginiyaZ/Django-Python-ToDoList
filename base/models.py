907f3aad8dd8fe8d826f2f84332813b3506b6ce6
Virginiya <93266719+VirginiyaZ@users.noreply.github.com>
2022-06-20 16:37:34 +0500

    Add files via upload

---
+++ base/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True )
+    title = models.CharField(max_length = 200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    due_date = models.DateTimeField(auto_now_add=False) 
+    
+
+    def __str__(self):
+        return self.title
+
+    def is_end_date(self):
+        return datetime.now() > self.created
+
+    class Meta:
+        ordering = ['complete']
+
